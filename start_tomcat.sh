53a8caa7468ec74cf55f8875f6bb75e0783cd326
RLDevOps <sandeep.ranganath@relevancelab.com>
2014-06-24 20:36:54 +0530

    Update start_tomcat.sh

--- start_tomcat.sh
+++ start_tomcat.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/opt/apache-tomcat-6.0.41/bin/startup.sh
 wget -O /opt/apache-tomcat-6.0.41/webapps/E2EMF.war $URL
+/opt/apache-tomcat-6.0.41/bin/startup.sh
 tail -f /opt/apache-tomcat-6.0.41/logs/catalina.out
