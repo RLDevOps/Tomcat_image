0d2aea9e798f7d2979b2acf44ea9ace1b9d96eb1
RLDevOps <sandeep.ranganath@relevancelab.com>
2014-06-25 11:58:00 +0530

    Delete start_tomcat.sh

--- start_tomcat.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget -O /opt/apache-tomcat-6.0.41/webapps/E2EMF.war http://9.9.9.11/nexus/service/local/repositories/$repo_name/content/$groupID/$artifactID/$version/$artifactID-$version.war
-/opt/apache-tomcat-6.0.41/bin/startup.sh
-tail -f /opt/apache-tomcat-6.0.41/logs/catalina.out
